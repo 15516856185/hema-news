27aab21094196892feb2dcc7f0a521d5f0117a15
七分醉 <1768645838@qq.com>
2020-08-27 14:35:31 +0800

    初始化了组件

---
+++ src/components/NyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>我是局部组件---{{msg}}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '123'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>用户页面
+  <span class='iconfont iconweixin'>{{age}}</span>
+  <myheader></myheader>
+  </div>
+</template>
+<script>
+import myheader from '../components/NyHeader'
+export default {
+  data () {
+    return {
+      age: 18
+    }
+  },
+  components: {
+    myheader
+  }
+}
+</script>
+<style lang="">
+</style>
